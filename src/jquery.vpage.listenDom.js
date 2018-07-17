06bb44305bdc16972667adcf5e0496041eea61ae
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2018-07-17 18:01:19 +0800

    簡化進階範例

--- src/jquery.vpage.listenDom.js
+++ src/jquery.vpage.listenDom.js
@@ -4,9 +4,11 @@
         $("body").on("click", "[data-vpage]", function (e){
             e.preventDefault();
             
+            // 取得標籤名稱與模型名稱
             var nodeName = e.currentTarget.nodeName;
             var modelName = $(this).attr("data-vpage");
 
+            // 若是超連結
             if (nodeName === "A"){
                 var href = $(this).attr("href");
             } else {
